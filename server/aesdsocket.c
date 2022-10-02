8d4d247da6945e6dd0fb6b528d4df8728eeff8b2
Dhiraj Bennadi <dhiraj.bennadi@colorado.edu>
2022-10-02 11:49:19 -0600

    Added Syslog

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -18,7 +18,11 @@
 #include <stdbool.h>
 #include <signal.h>
 
-#define PORT 9000  // the port users will be connecting to
+#include <syslog.h>
+
+// syslog(LOG_ERR , "Insufficient Arguments");
+
+#define PORT 9000 // the port users will be connecting to
 
 #define BUFFER_STD_SIZE 256
 
@@ -33,23 +37,24 @@ void sig_handler(int signum);
 
 bool deamonFlag = false;
 
-
 pid_t pid;
 
+// syslog
 
 int main(int argc, char **argv)
 {
 
+    openlog("ServersOpsDB", LOG_CONS, LOG_USER);
 
-    printf("Server Ops\n");
+    syslog(LOG_INFO, "Server Operations\n");
 
-
-    printf("Registering Callbacks for Signal Handlers\n");
+    syslog(LOG_INFO, "Registering Callbacks for Signal Handlers\n");
+    // printf("Registering Callbacks for Signal Handlers\n");
     signal(SIGINT, sig_handler);  // Register signal handler
     signal(SIGTERM, sig_handler); // Register signal handler
 
     /*File Operations*/
-    FILE *filePointer; 
+    FILE *filePointer;
 
     int bindSocket;
     int recvClient;
@@ -57,79 +62,85 @@ int main(int argc, char **argv)
 
     struct sockaddr_in serverAddress;
     struct sockaddr_in clientAddress;
-    //struct sockaddr clientAddress;
     int clientSocket;
     socklen_t addr_size;
-    
 
     /*Domain, Type, Protocol*/
-    serverSocket = socket(AF_INET , SOCK_STREAM, 0);
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(serverSocket < 0)
+    if (serverSocket < 0)
     {
         perror("[-] Socket Creation Failed\n");
         exit(-1);
     }
-    printf("[+] Server Socket Created\n");
+    syslog(LOG_INFO, "[+] Server Socket Created\n");
+    // printf("[+] Server Socket Created\n");
 
-    memset(&serverAddress , '\0' , sizeof(struct sockaddr_in));
-    memset(&clientAddress , '\0' , sizeof(struct sockaddr_in));
-
+    memset(&serverAddress, '\0', sizeof(struct sockaddr_in));
+    memset(&clientAddress, '\0', sizeof(struct sockaddr_in));
 
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
 
-    bindSocket = bind(serverSocket , (struct sockaddr_in *)&serverAddress , sizeof(struct sockaddr_in));
+    bindSocket = bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(struct sockaddr_in));
 
     if (bindSocket < 0)
     {
         perror("[-] Bind Failed\n");
         exit(-1);
     }
-    printf("[+] Bind Socket Completed\n");
 
+    syslog(LOG_INFO, "[+] Bind Socket Completed\n");
+    // printf("[+] Bind Socket Completed\n");
 
-    if(argc > 1)
+    if (argc > 2)
     {
-        printf("Number of Arguments = %d\n", argc);
-        for(int i = 0; i < argc; i++)
-        {
-            printf("%s\n", argv[i]);
+        perror("Additional Arguments received\n");
+        exit(-1);
+    }
 
-        }
-        if(!strcmp(argv[1] , "-d"))
+    if (argc == 2)
+    {
+        // printf("Number of Arguments = %d\n", argc);
+        // for(int i = 0; i < argc; i++)
+        // {
+        //     printf("%s\n", argv[i]);
+
+        // }
+        if (!strcmp(argv[1], "-d"))
         {
-            printf("Deamon Process Ignition\n");
+            syslog(LOG_INFO, "[+] Deamon Process Ignition\n");
+            // printf("Deamon Process Ignition\n");
             deamonFlag = true;
         }
     }
 
-
-    if(deamonFlag)
+    if (deamonFlag)
     {
-        pid = fork ();
+        pid = fork();
 
         if (pid == -1)
         {
-            printf("Forking Failed\n");
+            perror("Forking Failed\n");
             return -1;
         }
         else if (pid != 0)
         {
-            printf("Parent Process Exiting\n");
+            syslog(LOG_INFO, "[+] Parent Process Exiting\n");
+            // printf("Parent Process Exiting\n");
             exit(EXIT_SUCCESS);
         }
 
         if (setsid() == -1)
         {
-            printf("Set Sid Failed\n");
+            perror("Set Sid Failed\n");
             return -1;
         }
         /* set the working directory to the root directory */
         if (chdir("/") == -1)
         {
-            printf("Changing Working Directory Failed\n");
+            perror("Changing Working Directory Failed\n");
             return -1;
         }
         /* close all open files--NR_OPEN is overkill, but works */
@@ -145,17 +156,16 @@ int main(int argc, char **argv)
         dup(0);
         /* stderror */
         /* do its daemon thing... */
-        //return 0;
+        // return 0;
 
-        printf("Daemon Started\n");
+        syslog(LOG_INFO, "[+] Daemon Started\n");
+        // perror("Daemon Started\n");
     }
 
-
-
     /*Server Socket Listening to Connections*/
-    int listenRetVal = listen(serverSocket , 5);
+    int listenRetVal = listen(serverSocket, 5);
 
-    if(listenRetVal < 0)
+    if (listenRetVal < 0)
     {
         perror("[-] Listen Failed\n");
     }
@@ -165,15 +175,13 @@ int main(int argc, char **argv)
     // printf("Server Family : %d\n", serverAddress.sin_family);
     // printf("Server Port : %d\n", htons(serverAddress.sin_port));
 
-
-    //char *receiveFromClient;
+    // char *receiveFromClient;
     char *writebuffer;
 
     int totalBuffer = BUFFER_STD_SIZE;
     int currentSize = 0;
 
     char receiveFromClient[BUFFER_STD_SIZE];
-    
 
     int bytesToBeWritten = 0;
     int readBufferSize = 0;
@@ -182,180 +190,136 @@ int main(int argc, char **argv)
 
     int bytesToBeRead = 0;
 
-    
     int lseekValue = 0;
 
     fd = open("/var/tmp/aesdsocketdata", O_CREAT | O_RDWR | O_TRUNC, 0644);
 
-    while(1)
+    while (1)
     {
-        //printf("Size = %d\n", sizeof(struct sockaddr));
         addr_size = sizeof(clientAddress);
-        clientSocket = accept(serverSocket, (struct sockaddr_in *) &clientAddress, &addr_size);
+        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &addr_size);
 
-        printf("Client Socket = %d\n", clientSocket);
+        // printf("Client Socket = %d\n", clientSocket);
+        syslog(LOG_INFO, "[+] Client Socket = %d\n", clientSocket);
 
         totalBuffer = BUFFER_STD_SIZE;
         currentSize = 0;
 
-
         writebuffer = malloc(sizeof(char) * BUFFER_STD_SIZE);
 
+        // filePointer = fopen("dhiraj.txt", "w");
 
-        //filePointer = fopen("dhiraj.txt", "w");
-
-        if(clientSocket < 0)
+        if (clientSocket < 0)
         {
             perror("[-] Accept Failed\n");
             exit(-1);
         }
 
-        printf("Client Connected\n");
-        printf("Client Address : %s\n", inet_ntoa(clientAddress.sin_addr));
-        //printf("Client Family : %d\n", clientAddress.sin_family);
-        //printf("Client Port : %d\n", clientAddress.sin_port);
-
-        // receiveMsgFromClient();
-
-        // writeToFile();
-
-        // readFromFile();
-
-        // sendMsgToClient();
+        // printf("Client Connected\n");
+        syslog(LOG_INFO, "Accepted connection from : %s\n", inet_ntoa(clientAddress.sin_addr));
+        // printf("Client Family : %d\n", clientAddress.sin_family);
+        // printf("Client Port : %d\n", clientAddress.sin_port);
 
         bool exitLoop = true;
 
-
-        while(exitLoop)
+        while (exitLoop)
         {
             recvClient = recv(clientSocket, receiveFromClient, BUFFER_STD_SIZE, 0);
 
             if (recvClient == 0 || (strchr(receiveFromClient, '\n') != NULL))
             {
-                printf("Packet Completed\n");
+                syslog(LOG_INFO, "Packet Completed\n");
                 exitLoop = false;
             }
 
             if ((totalBuffer - currentSize) < recvClient)
             {
                 totalBuffer += recvClient;
-                writebuffer = (char *) realloc(writebuffer, sizeof(char) * totalBuffer);
+                writebuffer = (char *)realloc(writebuffer, sizeof(char) * totalBuffer);
             }
 
             memcpy(writebuffer + currentSize, receiveFromClient, recvClient);
             currentSize += recvClient;
         }
 
-        // do{
-
-        //     recvClient = recv(clientSocket, receiveFromClient, BUFFER_STD_SIZE, 0);
-
-        //     if (recvClient == 0 || (strchr(receiveFromClient, '\n') != NULL))
-        //     {
-        //         printf("Packet Completed\n");
-        //         exitLoop = false;
-        //     }
-
-        //     if ((totalBuffer - currentSize) < recvClient)
-        //     {
-        //         totalBuffer += recvClient;
-        //         writebuffer = (char *) realloc(writebuffer, sizeof(char) * totalBuffer);
-        //     }
-
-        //     memcpy(writebuffer + currentSize, receiveFromClient, recvClient);
-        //     currentSize += recvClient;
-
-
-        // } while(exitLoop == true);
-
-        // char *ptr = writebuffer;
-        // for(int i = 0; i < currentSize; i++)
-        // {
-        //     printf("%c", *ptr);
-        //     ptr++;
-        // }
-
-        //bytesToBeWritten = fwrite(writebuffer, sizeof(char) , currentSize , filePointer);
         bytesToBeWritten = write(fd, writebuffer, currentSize);
         lseekValue = lseek(fd, 0, SEEK_SET);
 
-        //int fseekValue = fseek(filePointer, 0, SEEK_END);
+        // int fseekValue = fseek(filePointer, 0, SEEK_END);
 
-        printf("lseekValue  = %d\n", lseekValue);
+        // printf("lseekValue  = %d\n", lseekValue);
 
-        //printf("Bytes To be Written = %d\n", bytesToBeWritten);
+        // printf("Bytes To be Written = %d\n", bytesToBeWritten);
 
         readBufferSize += currentSize;
 
-        printf("Current Size = %d\n", currentSize);
-        printf("Read Bytes = %d\n", readBufferSize);
+        syslog(LOG_INFO, "Current Size = %d\n", currentSize);
+        syslog(LOG_INFO, "Read Bytes = %d\n", readBufferSize);
 
-        readFromFile = (char*)malloc(sizeof(char) * readBufferSize);
+        readFromFile = (char *)malloc(sizeof(char) * readBufferSize);
 
-        if(readFromFile == NULL)
+        if (readFromFile == NULL)
         {
-            printf("Malloc Failed\n");
+            perror("[-] Malloc Failed\n");
         }
 
-        //bytesToBeRead = fread(readFromFile , sizeof(char) , readBufferSize, filePointer);
+        // bytesToBeRead = fread(readFromFile , sizeof(char) , readBufferSize, filePointer);
 
         bytesToBeRead = read(fd, readFromFile, readBufferSize);
 
-        printf("Bytes To be Read = %d\n", bytesToBeRead);
+        syslog(LOG_INFO, "Bytes To be Read = %d\n", bytesToBeRead);
 
+        int clientSendRetVal = send(clientSocket, readFromFile, bytesToBeRead, 0);
 
+        syslog(LOG_INFO, "Client Send Return Value = %d\n", clientSendRetVal);
 
-
-        /*Debug Details*/
-        /**/
-
-        int clientSendRetVal = send(clientSocket, readFromFile, bytesToBeRead , 0);
-
-
-        printf("Client Send Return Value = %d\n", clientSendRetVal);
-
-        if(clientSendRetVal < 0)
+        if (clientSendRetVal < 0)
         {
-            printf("Client Sending Failed\n");
+            perror("[-] Client Sending Failed\n");
         }
 
         free(readFromFile);
         free(writebuffer);
 
-        //close(serverSocket);
+        /*Closing Socket*/
+        // int closeSocketRetVal = close(serverSocket);
 
-        //close(fd);
+        // if (closeSocketRetVal < 0)
+        // {
+        //     perror("[-] Socket Closing Failed\n");
+        //     exit(-1);
+        // }
 
-
+        syslog(LOG_INFO, "Closed connection from : %s\n", inet_ntoa(clientAddress.sin_addr));
     }
-
 
     return 0;
 }
 
-
 void sig_handler(int signum)
 {
 
-    if(signum == SIGINT)
+    if (signum == SIGINT)
     {
-        printf("Dhiraj Bennadi\n");
-        printf("SIGINT Received\n");
-        close(fd);
-        close(serverSocket);
-        remove("/var/tmp/aesdsocketdata");
-        exit(-1);
+        syslog(LOG_INFO, "Caught signal = %d, exiting\n", SIGINT);
     }
-    else if(signum == SIGTERM)
+    else if (signum == SIGTERM)
     {
-        close(fd);
-        close(serverSocket);
-        remove("/var/tmp/aesdsocketdata");
-
-        printf("Dhiraj Bennadi\n");
-        printf("SIGTERM Received\n");
-        exit(-1);
+        printf("Caught signal = %d, exiting\n", SIGTERM);
     }
 
+    close(fd);
+    close(serverSocket);
+    remove("/var/tmp/aesdsocketdata");
 
+    close(STDOUT_FILENO);
+    close(STDIN_FILENO);
+    close(STDERR_FILENO);
+
+    syslog(LOG_INFO, "Signal Handler Completed\n");
+
+    /*Closing Syslog File*/
+    closelog();
+
+    exit(0);
 }
