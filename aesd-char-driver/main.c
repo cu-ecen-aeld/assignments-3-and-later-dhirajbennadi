851c9a25c6fcfaba80c21bdbc2b640f4fb491cad
Dhiraj Bennadi <dhiraj.bennadi@colorado.edu>
2022-11-03 19:14:12 -0600

    Driver

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -202,7 +202,7 @@ long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				}
 				else
 				{
-					printk(KERN_WARNING "Dhiraj Write Cmd Offset = %d", seekto.write_cmd_offset);
+					//printk(KERN_WARNING "Dhiraj Write Cmd Offset = %d", seekto.write_cmd_offset);
 
 					ret_val = aesd_adjust_file_offset(filp, seekto.write_cmd, seekto.write_cmd_offset);
 				}
